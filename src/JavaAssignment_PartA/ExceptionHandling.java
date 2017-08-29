afb8c631c8f3f4d3ef4c1f738589050fa0f7b34c
Jerry Mieles <jaymieles7@gmail.com>
2017-08-28 21:27:39 -0400

    Added comments

--- src/JavaAssignment_PartA/ExceptionHandling.java
+++ src/JavaAssignment_PartA/ExceptionHandling.java
@@ -4,11 +4,13 @@ import java.util.*;
 
 public class ExceptionHandling {
 	public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+		//Prompt the user for an array size
 		System.out.println("Enter the number of elements in the array:");
 		Scanner scanner = new Scanner(System.in);
 		try {
 			int size = scanner.nextInt();
-			if(size <0) {
+			//if the size is less than 1 then prompt for a valid size
+			if(size <1) {
 				boolean validSize = false;
 				while(!validSize) {
 					System.out.println("Number of elements must be greater than 0, reenter:");
@@ -18,11 +20,13 @@ public class ExceptionHandling {
 					}
 				}
 			}
+			//receive integers from the user
 			int[] array = new int[size];
 			System.out.println("Enter the elements in the array:");
 			for(int i = 0; i<size; i++) {
 				array[i] = scanner.nextInt();
 			}
+			//prompt for the index to output
 			System.out.println("Enter the index of the array element you want to access:");
 			int index = scanner.nextInt();
 			System.out.println("The array element at index " + index
